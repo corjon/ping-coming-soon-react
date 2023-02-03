a50edc5659937b4ebb62dc29a2eedddc6361b3e8
corjon <cordesjonathan@gmail.com>
2023-02-02 18:24:23 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Frontend Mentor - Ping coming soon page solution
+
+This is a solution to the [Ping coming soon page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Submit their email address using an `input` field
+- Receive an error message when the `form` is submitted if:
+	- The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
+	- The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"*
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+
+### What I learned
+
+- Simple form validation with useState
+
+### Useful resources
+
+- [React Docs](https://beta.reactjs.org/learn/managing-state) - Managing state
+
+## Author
+
+- Frontend Mentor - [@corjon](https://www.frontendmentor.io/profile/corjon)
---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import './App.css';
+import logo from './assets/logo.svg';
+import dashboard from './assets/illustration-dashboard.png';
+
+function Form() {
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(null);
+
+  function handleChange(e) {
+    setEmail(e.target.value);
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    setIsValid(re.test(String(e.target.value).toLowerCase()));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email === '') {
+      setIsValid(false);
+    }
+    else if (isValid) {
+      alert('valid email: ' + email);
+      // submit form
+      setEmail('');
+      setIsValid(null);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={email} onChange={handleChange} placeholder="Your email address..." 
+        style={{borderColor: isValid === null ? 'initial' : isValid ? 'initial' : '#FF5466'}} 
+      />
+      {isValid === false && email === '' ? (
+        <p class="error">Whoops! It looks like you forgot to add your email</p> 
+      ) : (
+        isValid === false && <p class="error">Please provide a valid email address</p>
+      )}
+      <input type="submit" value="Notify Me" />
+    </form>
+  );
+}
+
+export default function App() {
+  return (
+    <main>
+      <img src={logo} alt="company logo" class="logo" />
+      <h1>We are launching soon!</h1>
+      <p class="cta">Subscribe and get notified</p>
+      <Form />
+      <img src={dashboard} alt="dashboard with graphs" class="dashboard" />
+
+      <div class="socials">
+        <a href="#" aria-label="facebook" class="fa fa-facebook-f"></a>
+        <a href="#" aria-label="twitter" class="fa fa-twitter"></a>
+        <a href="#" aria-label="instagram" class="fa fa-instagram"></a>
+      </div>
+  
+      <p class="copy">&copy; Copyright Ping. All rights reserved.</p>
+    </main>
+  );
+}
